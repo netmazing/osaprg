052948b43f6711eeb06cd2c10ef91dbc6643121e
Dev <p.trojnacki@o2.pl>
2019-10-31 16:14:56 +0100

    error page2

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,25 @@
-import React from "react"
+import React from 'react';
+import Layout from '../components/Layout';
+import '../css/404.css';
+// import SEO from '../components/SEO';
 
-// import Layout from "../components/Layout"
+const page404 = () => {
+  return (
+    <Layout>
+      {/* <SEO title="Błąd" /> */}
+      <div className="error-page">
+        <div className="error-page__section">
+          <p className="error-page__info">Nie ma takiej strony</p>
+          <a href="/" className="error-page__btn">Strona główna</a>
+        </div>
+        
+        <div className="error-page__section">
+          <p className="error-page__info">There is no such page</p>
+          <a href="/en" className="error-page__btn">Back to Homepage</a>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
-const NotFoundPage = () => (
-  // <Layout>
-    <h1>NOT FOUND</h1>
-  // </Layout>
-)
-
-export default NotFoundPage
+export default page404
